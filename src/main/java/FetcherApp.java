2b78f41e95862fe52bc03374e864faa88d5f1e05
apushkin <apushkin@ipmks.com>
2018-08-06 13:28:43 +0300

    Added 1st JSON realization of the GitLab Issue

--- src/main/java/FetcherApp.java
+++ src/main/java/FetcherApp.java
@@ -1,12 +1,25 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
+import pojo.GitlabIssue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FetcherApp {
     private static final Logger logger = LoggerFactory.getLogger(FetcherApp.class);
+    private static String urlStr = "https://gitlab.ipmks.off/api/v4/issues?scope=all&milestone=4.7.29-DEV5&per_page=50";
 
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
-
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<List<GitlabIssue>> entity = new HttpEntity<>(new ArrayList<>(), httpHeaders);
+        httpHeaders.add("Private-Token", args[0]);
+        restTemplate.exchange(urlStr, HttpMethod.GET, entity, ArrayList.class);
+        List<GitlabIssue> issues = restTemplate.getForObject("https://gitlab.ipmks.off/api/v4/issues?scope=all&milestone=4.7.29-DEV5&per_page=50", ArrayList.class);
+        System.out.println(1);
     }
 }
